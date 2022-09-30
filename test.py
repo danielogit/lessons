0c911c915385e4b500f303985a78e2e5c542961e
Daniel Olszewski <daniel.olszewski@endava.com>
2022-09-30 12:30:02 +0200

    Reset commits

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import unittest
+from fractions import Fraction
 
 
 def test_sum():
@@ -16,6 +17,22 @@ class TestSum(unittest.TestCase):
 
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
 
 
 if __name__ == "__main__":
